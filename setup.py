890afbe78a6d9f2b29a29f516cebab5b321280bd
ryanwintersusc <44385625+ryanwintersusc@users.noreply.github.com>
2019-01-30 13:36:04 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ryanwinterstest1221',
+      version='0.1',
+      description='This is just a test, go away',
+      url='https://github.com/ryanwintersusc',
+      author='Ryan Winters',
+      author_email='ryanwint@usc.edu',
+      license='MIT',
+      packages=['ryanwinterstest1221'],
+      zip_safe=False)
